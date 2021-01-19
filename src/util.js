84bf362b3422c90dcc989906a0a569514e4db19b
luizffdemoraes <lffm1994@gmail.com>
2021-01-19 00:51:53 -0300

    The Secrets of JavaScript Syntax

---
+++ src/util.js
@@ -0,0 +1,16 @@
+class Util {
+  static #defaultFormat = Intl.NumberFormat("pt-br", {
+    currency: "BRL",
+    style: "currency"
+  })
+  static formatCurrency(value) {
+    return this.#defaultFormat.format(value)
+  }
+  static unFormatCurrency(value){
+    // R$ 1000,10 - 100010
+    return Number(value.replace(/\D/g, '')) / 100
+    
+  }
+}
+
+module.exports = Util
